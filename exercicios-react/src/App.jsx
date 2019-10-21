e5832b758674051163835460c01cfb2557d7ef32
Carlos Silva <carlos.silva@indigosoft.com.br>
2019-10-21 13:44:58 -0300

    FirstComponent + Second

---
+++ exercicios-react/src/App.jsx
@@ -0,0 +1,8 @@
+import React, { Fragment } from 'react'
+
+import FirstArrowComponent from './components/FirstArrowComponent'
+
+export default () => 
+    <Fragment>
+        <FirstArrowComponent />
+    </Fragment>
---
+++ exercicios-react/src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function primeiro() {
+    return (
+        <h1>First Component</h1>
+    )
+}
+
+export default primeiro
